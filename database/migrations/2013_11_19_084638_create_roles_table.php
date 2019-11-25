b5204e7a17ecde19d149cd74c93bd348dfedef99
Unknown <kyantempelaars@gmail.com>
2019-11-25 10:01:41 +0100

    roles fixed

--- database/migrations/2013_11_19_084638_create_roles_table.php
+++ database/migrations/2013_11_19_084638_create_roles_table.php
@@ -15,7 +15,7 @@ class CreateRolesTable extends Migration
     {
         Schema::create('roles', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->integer('role');
+            $table->text('role');
             $table->timestamps();
         });
     }
