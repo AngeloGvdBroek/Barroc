76597abf7986a4b8432fe27a5c9faf872d948ce8
Unknown <angelovandenbroek@hotmail.com>
2019-11-25 09:45:43 +0100

    Register button removed

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -45,11 +45,7 @@
                             <li class="nav-item">
                                 <a class="wh-text link spacing link-removal " href="{{ route('login') }}">{{ __('Login') }}</a>
                             </li>
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="wh-text link link-removal " href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
+                          
                         @else
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
